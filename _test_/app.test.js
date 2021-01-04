d03c02b62ae871e62a6f14c90195da9564d705e1
mirsahib <mirsahib24@gmail.com>
2021-01-04 19:43:01 +0600

    :tada: initial commit

---
+++ _test_/app.test.js
@@ -0,0 +1,36 @@
+// Add import statements
+
+const { app, server, mongoose } = require("../app.js"); // runs app.js
+const Article = require("./../article.model"); // imports Article model
+const request = require("supertest"); // needed to make API requests
+
+describe("When the article CRUD server is running", () => {
+  // Add individual test cases
+  it("should return 200 response if POST request receives proper article data", async () => {
+    const res = await request(app)
+      .post("/article/new")
+      .send({ title: "test", content: "test" });
+    expect(res.status).toBe(200);
+    expect(typeof res.body).toEqual("object");
+  });
+
+  it("should return 400 response if POST request data is missing the article title", async () => {
+    const res = await request(app)
+      .post("/article/new")
+      .send({ content: "test" });
+    expect(res.status).toBe(400);
+    expect(typeof res.body).toEqual("object");
+  });
+  //test driven implementation
+  it("should return 200 response and all the articles in DB on GET request to '/article/all'", async () => {
+    const res = await request(app).get("/articles/all");
+    expect(res.status).toBe(200);
+    expect(res.body.length).toEqual(1);
+  });
+
+  afterAll(async () => {
+    await server.close();
+    await mongoose.connection.db.dropDatabase();
+    await mongoose.connection.close();
+  });
+});
---
+++ app.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const express = require("express");
+const Article = require("./article.model");
+const bodyParser = require("body-parser");
+
+const app = express();
+const server = http.Server(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//database code
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+let dbURL =
+  "mongodb://localhost:27017/" +
+  (process.env.NODE_ENV === "test" ? "testDB" : "realDB");
+dbURL = process.env.NODE_ENV === "prod" ? process.env.db : dbURL;
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on("error", function (err) {
+  console.log(err);
+});
+//end of database
+
+//api call
+app.post("/article/new", function (request, response) {
+  var newArticle = new Article(request.body);
+  newArticle.save(function (err, data) {
+    if (err)
+      return response.status(400).json({ error: "Could not save article" });
+    return response
+      .status(200)
+      .json({ message: "Article created successfully" });
+  });
+});
+
+app.get("/articles/all", function (request, response) {
+  Article.find({}, function (err, data) {
+    response.status(200).json(data);
+  });
+});
+
+//server running
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", () => {
+  console.log("Server running");
+});
+module.exports = { app, server, mongoose };
